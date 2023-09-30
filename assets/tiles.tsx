0d2e6b68ef03ad2a2321d4ea111cb46eb1e1e588
Rémi Vanicat <vanicat@debian.org>
2023-09-30 17:55:41 +0200

    some animation

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,6 +1,22 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="8" tileheight="8" tilecount="32" columns="8">
- <image source="tiles.png" width="64" height="32"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="8" tileheight="8" tilecount="40" columns="8">
+ <image source="tiles.png" width="64" height="40"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="7" duration="100"/>
+  </animation>
+ </tile>
  <tile id="16">
   <properties>
    <property name="type" value="wall"/>
